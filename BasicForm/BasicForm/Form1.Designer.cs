7c32569e98563385a5f5b445b53ae45c16da40f6
rickchen0983 <r2606325@gmail.com>
2019-07-06 16:52:36 +0800

    Issue #1 Add two points' coordinate.

--- BasicForm/BasicForm/Form1.Designer.cs
+++ BasicForm/BasicForm/Form1.Designer.cs
@@ -33,6 +33,12 @@
             this.button2 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // shapeContainer2
@@ -49,14 +55,14 @@
             // lineShape2
             // 
             this.lineShape2.Name = "lineShape2";
-            this.lineShape2.X1 = 301;
-            this.lineShape2.X2 = 464;
-            this.lineShape2.Y1 = 129;
-            this.lineShape2.Y2 = 129;
+            this.lineShape2.X1 = 307;
+            this.lineShape2.X2 = 216;
+            this.lineShape2.Y1 = 138;
+            this.lineShape2.Y2 = 138;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(125, 167);
+            this.button2.Location = new System.Drawing.Point(213, 372);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
@@ -66,21 +72,77 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(88, 73);
+            this.textBox2.Location = new System.Drawing.Point(349, 271);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(180, 22);
             this.textBox2.TabIndex = 3;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(88, 107);
+            this.textBox3.Location = new System.Drawing.Point(349, 305);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(180, 22);
             this.textBox3.TabIndex = 4;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(297, 280);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "X2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(297, 316);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Y2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 318);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Y1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 282);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "X1";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(71, 307);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(180, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(71, 273);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(180, 22);
+            this.textBox5.TabIndex = 7;
+            // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(562, 420);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button2);
@@ -103,6 +165,12 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
     }
 }
 
--- BasicForm/BasicForm/Form1.cs
+++ BasicForm/BasicForm/Form1.cs
@@ -16,28 +16,40 @@ namespace BasicForm
         {
             InitializeComponent();
         }
-        double l = 0;
+        double lx1 = 0;
+        double ly1 = 0;
+        double lx2 = 0;
+        double ly2 = 0;
         private void Button2_Click(object sender, EventArgs e)
         {
-            double? x=0;
-            double? y=0;
-            if (x == null | y == null)
+            double? x1 = 0;
+            double? y1 = 0;
+            double? x2 = 0;
+            double? y2 = 0;
+            if (x1 == null | y1 == null | x2 == null | y2 == null)
             {
                 MessageBox.Show("failed");
             }
             else
             {
-                x = double.Parse(textBox2.Text);
-                lineShape2.X2 = (int)x ;
-                y = double.Parse(textBox3.Text);
-                lineShape2.Y2 = (int)y ;
+                x1 = double.Parse(textBox5.Text);
+                lineShape2.X1 = (int)x1;
+                y1 = double.Parse(textBox4.Text);
+                lineShape2.Y1 = (int)y1;
+                x2 = double.Parse(textBox2.Text);
+                lineShape2.X2 = (int)x2 + 270;
+                y2 = double.Parse(textBox3.Text);
+                lineShape2.Y2 = (int)y2 + 150;
             }
                 
         }
 
         private void Form1_Load_1(object sender, EventArgs e)
         {
-            l = lineShape2.Y2;
+            lx1 = lineShape2.X1;
+            ly1 = lineShape2.Y1;
+            lx2 = lineShape2.X2;
+            ly2 = lineShape2.Y2;
         }
 
     }
